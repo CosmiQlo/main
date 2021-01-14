46c8d043f4998f4ac32ec7b7590e85a3319e9a1c
Laura Kathryn Gilbert <laura.kate.gilbert@gmail.com>
2021-01-14 16:58:58 -0500

    made link apply to image as well as name of product, and added Route for /products

--- client/components/products.js
+++ client/components/products.js
@@ -18,8 +18,12 @@ export class Products extends React.Component {
           return (
             // this could be where we make the product.name a LINK that goes to single product view, etc.
             <div key={product.id}>
-              <Link to={`/products/${product.id}`}>{product.name}</Link>
-              <img src={product.imageUrl} />
+              <Link to={`/products/${product.id}`}>
+                <div>
+                  <h1>{product.name}</h1>
+                  <img src={product.imageUrl} />
+                </div>
+              </Link>
             </div>
           )
         })}
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, singleProduct} from './components'
+import {Login, Signup, UserHome, Products, singleProduct} from './components'
 import {me} from './store'
 
 /**
@@ -22,7 +22,8 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/home" component={UserHome} />
-        <Route path="/products/:productId" component={singleProduct} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/products/:productId" component={singleProduct} />
 
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
