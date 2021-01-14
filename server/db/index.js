c7501424b132f0464e224a009a59cb7f2acd98d7
Spandana Nallapati <drsnallapati@gmail.com>
2021-01-14 16:27:23 -0500

    placed the associations in the correct place

--- server/db/index.js
+++ server/db/index.js
@@ -1,35 +1,6 @@
-const Sequelize = require('sequelize')
 const db = require('./db')
 
-// bring in the models
-const Product = require('./models/product')
-const User = require('./models/user')
-const Order = require('./models/order')
-const orderProduct = require('./models/orderProduct')
+//register the models
+require('./models')
 
-//Product - Order Associations
-Product.belongsToMany(Order, {
-  through: orderProduct
-})
-
-Order.belongsToMany(Product, {
-  through: orderProduct
-})
-
-//User and Order Associations
-
-User.hasMany(Order)
-Order.belongsTo(User)
-
-//Product and User Associations
-
-Product.belongsToMany(User, {through: 'ProductUser'})
-User.belongsToMany(Product, {through: 'ProductUser'})
-
-module.exports = {
-  db,
-  Product,
-  User,
-  Order
-  // orderProduct,
-}
+module.exports = db
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,7 @@
-const User = require('./user')
 const Product = require('./product')
-
+const User = require('./user')
+const Order = require('./order')
+const orderProduct = require('./orderProduct')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -14,7 +15,30 @@ const Product = require('./product')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+//Product - Order Associations
+
+Product.belongsToMany(Order, {
+  through: orderProduct
+})
+
+Order.belongsToMany(Product, {
+  through: orderProduct
+})
+
+//User and Order Associations
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+//Product and User Associations
+
+Product.belongsToMany(User, {through: 'ProductUser'})
+User.belongsToMany(Product, {through: 'ProductUser'})
+
 module.exports = {
   User,
-  Product
+  Product,
+  Order,
+  orderProduct
 }
