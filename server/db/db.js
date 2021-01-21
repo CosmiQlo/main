272285f4b3e1918aec077fe17ad4f97c0f743f84
Laura Kathryn Gilbert <laura.kate.gilbert@gmail.com>
2021-01-21 11:46:39 -0500

    photos should render in appropriate aspect ratio

--- server/db/db.js
+++ server/db/db.js
@@ -6,18 +6,18 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false,
+    logging: false
 
     // starting below this line, I have added code>>>
     // UNCOMMENT ONLY WHEN DEPLOYING TO HEROKU, KEEP THIS COMMENTED OUT WHEN RUNNING LOCALLY-->
-    operatorsAliases: false,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false
-      }
-    }
+    // operatorsAliases: false,
+    // dialect: 'postgres',
+    // protocol: 'postgres',
+    // dialectOptions: {
+    //   ssl: {
+    //     rejectUnauthorized: false
+    //   }
+    // }
     // code above is what I added^^^
   }
 )
